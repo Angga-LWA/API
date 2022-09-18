d64308b1167372ba9f944cb9f0fe13c7d1255a23
Yoga Anggara <doctorbean31@gmail.com>
2022-09-18 22:45:34 +0700

    connection database
    koneksi database dengan file .env

--- node-rest-api/app.js
+++ node-rest-api/app.js
@@ -1,13 +1,24 @@
 //definisi
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 //routes
 app.get('/', (req, res)=> {
     res.send('Routes running..!!')
 })
 
-//listen
-app.listen(3003, () => {
-    console.log('Server running in 3003..!!')
+//connection to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+let db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Database connect Error!'))
+db.once('open', () => {
+    console.log('Database is Connected!')
+})
+
+//listen, TANDA `` untuk memanggil variabel dlm string
+app.listen(process.env.PORT, () => {
+    console.log(`Server running in ${process.env.PORT}`)
 })
